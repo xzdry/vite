5a9b7d0ce5aabadea0d2aa8e6fa49aa1a99366b5
xzdry <dry@justdry.net>
2023-04-06 16:21:51 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React, { Suspense, useMemo } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
+const App = () => {
+  console.log("rerender");
+  const Page = useMemo(() => {
+    return React.lazy(() => {
+      return import("../modules/tmp.json").then((data) => {
+        return { default:()=><div>{data.version}</div> };
+      });
+    });
+  }, []);
+  return <Page />;
+};
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<div>asd</div>}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+//   <React.StrictMode>
+//     <Suspense
+//       fallback={
+//         <div>asd</div>
+//       }
+//     >
+//       <App />
+//     </Suspense>
+//   </React.StrictMode>
+// );
